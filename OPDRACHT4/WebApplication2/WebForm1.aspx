c3a579f2b45723934e01653d9fc078df8afa804c
burrowyacookie <yukselgs8@hotmail.com>
2017-03-27 10:03:08 +0200

    Opdracht4

---
+++ OPDRACHT4/WebApplication2/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #FF0000;
+        }
+        </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="color: #000000; background-color: #CCCCCC">
+    
+        <h1 class="auto-style1">Het aftellen is begonnen.</h1>
+        <br />Het is vandaag:<br />
+        <asp:Label ID="litVandaag" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+        Het is nu:<br />
+        <asp:Label ID="litTijd" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br /><strong>Wanneer is het Pasen?</strong><br />
+        <br />
+        <asp:Label ID="litPasen" runat="server"></asp:Label>
+    
+        <br />
+        <asp:Button ID="btnLaden" runat="server" OnClick="btnLaden_Click" Text="verversen" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ OPDRACHT4/WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnActie_Click(object sender, EventArgs e)
+        {
+
+            DateTime nu = DateTime.Now;
+            TimeSpan objTimeSpan = new TimeSpan(2, 10, 15, 3);
+            DateTime toekomst = nu.Add(objTimeSpan);
+            litVandaag.Text = toekomst.ToString();
+
+
+        }
+
+        protected void btnLaden_Click(object sender, EventArgs e)
+        {
+
+           
+            DateTime Nu = DateTime.Now;
+            string NuDatum = " " + Nu;
+            Response.Write(DateTime.Now.ToString("hh.mm"));
+            DateTime Tijd = new DateTime(2017, 4, 16, 0, 0, 0);
+            string TijdDatum = " " + Tijd;
+
+            //Trek nu van pasen af
+            TimeSpan objTimeSpan = Tijd.Subtract(Nu);
+
+            //Aantal uren
+            int dagen = objTimeSpan.Days;
+            int uren = objTimeSpan.Hours;
+            int minuten = objTimeSpan.Minutes;
+            int seconden = objTimeSpan.Seconds;
+            int totaaluren = dagen * 24 + uren;
+
+            string PasenDatum = "dagen: " + dagen + "<br>" +
+                "uren: " + uren + "<br>" +
+                "minuten: " + minuten + "<br>" +
+                "seconden: " + seconden;
+
+            litVandaag.Text = NuDatum;
+            litTijd.Text = TijdDatum;
+            litPasen.Text = PasenDatum;
+
+        }
+    }
+}
+
+
+  
